13937a7d20f38cd530670f4b7cf196965a1d4eb1
Valentin Laptop <valentin.beaumelle@gmail.com>
2024-03-05 16:06:08 +0100

    Initialisation backend

---
+++ ptutservicesprevisionnels/src/main/java/com/valentinisis/ptutservicesprevisionnels/PtutservicesprevisionnelsApplication.java
@@ -0,0 +1,13 @@
+package com.valentinisis.ptutservicesprevisionnels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtutservicesprevisionnelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtutservicesprevisionnelsApplication.class, args);
+	}
+
+}
---
+++ ptutservicesprevisionnels/src/test/java/com/valentinisis/ptutservicesprevisionnels/PtutservicesprevisionnelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valentinisis.ptutservicesprevisionnels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtutservicesprevisionnelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
